9d15adc73acefc27e5e988960fc05d0241b21841
Udbhavsomani <udbhavsomani18@gmail.com>
2022-01-17 03:42:47 +0530

    test changes

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,19 @@ function App() {
   return (
     <Router>
       <div className="App">
-        Fib Calculator - Kubernetes - Version 2
-        <br />
-        <Link to="/">Home</Link>
-        <br />
-        <Link to="/otherpage">Other Page</Link>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Fib Calculator - Kubernetes - Version 2
+          </a>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
         <div>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
